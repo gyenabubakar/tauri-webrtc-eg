d9b19cb02bbb64353c57f1f959678cf888a064fa
Gyen Abubakar <indehyde@gmail.com>
2022-12-27 23:06:31 +0000

    bootstrapped sveltekit app.

---
+++ src/lib/Home.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  let url = '';
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<div>
+  <form
+    class="row"
+    on:submit|preventDefault={() => {
+      dispatch('submit', url);
+    }}
+  >
+    <input id="greet-input" placeholder="Enter meeting URL..." bind:value={url} />
+    <button type="submit"> Start Call </button>
+  </form>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Greet from '$lib/Home.svelte';
+  import CallRoom from '$lib/CallRoom.svelte';
+
+  let url: string | undefined;
+</script>
+
+<div class="row">
+  <Greet
+    on:submit={e => {
+      url = e.detail;
+    }}
+  />
+
+  {#if url}
+    <CallRoom {url} />
+  {/if}
+</div>
+
+<style>
+</style>
